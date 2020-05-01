582e9c768afb39a06581db3f48593fe3d2f9b9ad
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2020-05-01 15:38:53 -0400

    Methodize files

--- src/adamhs1997/infinitesizedbuffer/InfiniteSizedBuffer.java
+++ src/adamhs1997/infinitesizedbuffer/InfiniteSizedBuffer.java
@@ -18,75 +18,143 @@ When buffering new data:
 --As soon as head ptr passes x% threshold, have bkgd thread go thorugh and write that data.
 --Lock the buffer to prevent new writes, then move new data to start
 --Reset head ptr to end of current data, overwrite leftover data in array
-
-Remember to delete log file upon exit or restart!!!--Don't want it to sit too long on device
  */
-public class InfiniteSizedBuffer {
+public class InfiniteSizedBuffer implements AutoCloseable {
 
-    // Create the buffer
-    static double[] d = new double[10];
-    static int headPtr = 0;
-    static int globalSize = 0;
-    static double dumpPoint = 0.9; // %
-    static int bufSize = 50; // Should be (effectively) INF in the production code to support arbitrary length buffer
-    static String fnameBase = "balls";
-    static int fileCtr = 0;
+    private double[] buffer;     // array to back the buffer
+    private int headPtr;         // current position in true buffer array
+    private int bufferSize;      // number of elements in buffer
+    private double dumpPoint;    // percentage of size at which we begin dumping
+    private int fileCtr;         // number of data files
+    private boolean writeMode;   // determine if user is reading or writing
+    private boolean currentOut;  // determine if current data is out of memory
+    final static String FNAME_BASE = "data_bin/buf_data";
+
+    InfiniteSizedBuffer(int inMemSize, double dumpPct) {
+        buffer = new double[inMemSize];
+        headPtr = 0;
+        bufferSize = 0;
+        dumpPoint = dumpPct / 100;
+        fileCtr = 0;
+
+        // Create parent directory if it doesn't exist
+        File f = new File(FNAME_BASE);
+        if (!f.getParentFile().exists()) {
+            f.mkdirs();
+        }
+    }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        // Write into buffer
-        for (int i = 0; i < bufSize; i++) {
-            d[headPtr] = i;
-            globalSize++;
-            if (++headPtr > dumpPoint * d.length) {
-                dumpData();
-            }
-        }
-
-        fileCtr--;
-        // Read buffer back, reading file data when we hit uncached stuff
-        for (int k = globalSize; k > 0; k--) {
-            // Retrieve more data when we need it
-            if (headPtr == 0) {
-                DataInputStream dis = new DataInputStream(
-                    new FileInputStream(fnameBase + fileCtr-- + ".bin"));
-                while (true) {
-                    try {
-                        d[headPtr] = dis.readDouble();
-                        headPtr++;
-                    } catch (EOFException e) {
-                        break;
-                    }
-                }
-                dis.close();
+        try (InfiniteSizedBuffer isb = new InfiniteSizedBuffer(10, 90)) {
+            int bufSize = 50;
+            // Write into buffer
+            for (int i = 0; i < bufSize; i++) {
+                isb.writeData(i);
             }
 
-            System.out.println(d[headPtr - 1]);
+            for (int i = isb.getBufferSize(); i > 0; i--) {
+                System.out.println(isb.readData());
+            }
+        }
+    }
 
-            headPtr--;
+    public void writeData(double data) {
+        writeMode = true;
+
+        // If most current data is not in memory, load it back in
+        if (currentOut) {
+            loadBlock(FNAME_BASE + "x.bin");
+            currentOut = false;
         }
 
-        // Clean up files
-        new File(".").listFiles(f -> {
+        // Write given data
+        buffer[headPtr] = data;
+        bufferSize++;
+
+        if (++headPtr > dumpPoint * buffer.length) {
+            dumpData();
+        }
+    }
+
+    public double readData() {
+        writeMode = false;
+
+        // Retrieve more data when we need it
+        if (headPtr == 0) {
+            // If the current data is still in, must evict it
+            if (!currentOut) {
+                currentOut = true;
+                writeInMemData();
+                fileCtr--;
+            }
+
+            loadBlock(FNAME_BASE + fileCtr-- + ".bin");
+        }
+
+        // Update head pointer location
+        headPtr--;
+
+        return buffer[headPtr];
+    }
+
+    public int getBufferSize() {
+        return bufferSize;
+    }
+
+    @Override
+    public void close() {
+        new File(FNAME_BASE).getParentFile().listFiles(f -> {
             if (f.getName().endsWith(".bin")) f.delete();
             return true;
         });
     }
 
-    private static void dumpData() throws IOException {
+    private void dumpData() {
         // Dump the data in the buffer to the file
-        DataOutputStream dos = new DataOutputStream(new FileOutputStream(
-                fnameBase + fileCtr++ + ".bin"));
-        int stopIdx = (int) (dumpPoint * d.length);
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(
+          FNAME_BASE + fileCtr++ + ".bin"))) {
+            // Write out anything in the buffer
+            int stopIdx = (int) (dumpPoint * buffer.length);
+            for (int i = 0; i < stopIdx; i++) {
+                dos.writeDouble(buffer[i]);
+            }
 
-        for (int i = 0; i < stopIdx; i++) {
-            dos.writeDouble(d[i]);
+            // Move any data at end of array to start
+            headPtr = buffer.length - stopIdx;
+            for (int i = stopIdx; i < buffer.length; i++) {
+                buffer[i - stopIdx] = buffer[i];
+            }
+        } catch (IOException exc) {
+            System.err.println("Failed to dump data!");
         }
+    }
 
-        // Move any data at end of array to start
-        headPtr = d.length - stopIdx;
-        for (int i = stopIdx; i < d.length; i++) {
-            d[i - stopIdx] = d[i];
+    private void writeInMemData() {
+        try (DataOutputStream dos = new DataOutputStream(
+          new FileOutputStream(FNAME_BASE + "x.bin"))) {
+            // Write out anything in the buffer
+            for (int i = 0; i < headPtr; i++) {
+                dos.writeDouble(buffer[i]);
+            }
+        } catch (IOException exc) {
+            System.err.println("Failed to dump data!");
         }
-        dos.close();
+    }
+
+    private void loadBlock(String blockName) {
+        try (DataInputStream dis = new DataInputStream(
+          new FileInputStream(blockName))) {
+            headPtr = 0;
+            while (true) {
+                try {
+                    buffer[headPtr] = dis.readDouble();
+                    headPtr++;
+                } catch (EOFException e) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Could not read data!");
+        }
     }
 }
